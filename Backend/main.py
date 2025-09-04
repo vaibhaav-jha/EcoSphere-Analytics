bdbb7efa341c338fbd08c464fae33352cf464b71
vaibhaav-jha <134255330+vaibhaav-jha@users.noreply.github.com>
2025-09-04 19:44:08 -0400

    commit

--- Backend/main.py
+++ Backend/main.py
@@ -5,3 +5,7 @@ import fredapi as fred
 # we will work tomorrow in the cafe and at home. yes. Check my code now in datafetcher.py pls. 
 # yes just wait.
 #Vaibhaav i finished the code for data_fetcher.py, so just check it now or whenever today
+#I commited the changes. Jus tell me if you are reading.
+#I am commiting data_fetcher.py right now, but how do i commit again? I already did it.
+#Oh ok, good night see you tomorrow at school. 
+#Yes see you bye. 
