cac4e4227c5a57360a08a66b4339d3064baf9bce
vaibhaav-jha <134255330+vaibhaav-jha@users.noreply.github.com>
2025-09-05 19:19:55 -0400

    commit

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib as plt
 import fredapi as fred
 # we will work tomorrow in the cafe and at home. yes. Check my code now in datafetcher.py pls. 
-# yes just wait.
+# yes just wait. 
 #Vaibhaav i finished the code for data_fetcher.py, so just check it now or whenever today
 #I commited the changes. Jus tell me if you are reading.
 #I am commiting data_fetcher.py right now, but how do i commit again? I already did it.
